97b6ad06e56783ef43af8094464774940c2bb987
Archange16 <asathoud16@gmail.com>
2024-11-29 19:59:46 +0100

    first commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"t4vcUOxjs63svANMA/oocQtwbzz+XY7z+RX8e6+YlRE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
